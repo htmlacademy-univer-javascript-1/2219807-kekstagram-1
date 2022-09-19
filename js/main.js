d5ee78cfe838e69347cfe2a3fb9bc39bab83585b
mihail323i21 <mihail323i21@gmail.com>
2022-09-20 00:18:40 +0500

    fixxed changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-let getRandomNumber = (min, max) => {
-    if (min >= max) return NaN
-
-    
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
   }
+  if (min < max) {
+    [max,min] = [min, max];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-let isMaxLength = (text, length) => {
-    if (text.length >= length) return false;
-    return true;
-}
+const isMaxLength = (text, length) => (text.length >= length);
