9e7a41ef0dcfdde52e5561d50836a47733866949
mihail323i21 <mihail323i21@gmail.com>
2022-10-27 19:05:34 +0500

    module5-task1
    added modules

---
+++ js/util.js
@@ -0,0 +1,13 @@
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min < max) {
+    [max,min] = [min, max];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const isMaxLength = (text, length) => (text.length >= length);
+
+export {getRandomNumber};
