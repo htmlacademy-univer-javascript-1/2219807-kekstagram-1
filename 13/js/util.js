459dbada6f4db0cbf1da9fd367ee6f5c1242adbc
Keks <keks@htmlacademy.ru>
2022-12-13 10:58:34 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/util.js
@@ -0,0 +1,36 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElementFromArray = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+const checkStringLength = (string, length) => string.length <= length;
+
+const uniqueNumberGenerator = () => {
+  let prevNumber = 0;
+
+  return () => ++prevNumber;
+};
+
+const isEscapeKey = (event) => event.key === 'Escape';
+
+const declensionComments = (numberComments) => {
+  const ones = numberComments % 10;
+  const tens = numberComments / 10 % 10;
+  if (ones === 1 && tens === 0 || (tens >= 2 && ones === 1)) {
+    return ' комментария';
+  }
+  return ' комментариев';
+};
+
+export {
+  getRandomPositiveInteger,
+  getRandomElementFromArray,
+  checkStringLength,
+  uniqueNumberGenerator,
+  isEscapeKey,
+  declensionComments
+};
