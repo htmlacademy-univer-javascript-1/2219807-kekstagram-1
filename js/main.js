0add523829560fb63b9fe2bba1ad422a9147f785
mihail323i21 <mihail323i21@gmail.com>
2022-12-18 13:37:44 +0500

    refactoring 11-1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-import './util.js';
-import {createImages} from './data.js';
 import {createThumbnails} from './renderCards.js';
 import {setUserFormSubmit, closeUploadFileForm} from './form.js';
 import {getDataFrom} from './api.js';
-import './big-pictures.js';
 import {getSuccess, getError} from './alerts.js';
 
-createThumbnails(createImages()); // для того чтобы показать НОРМАЛЬНУЮ работу кнопки показа 5 комментариев
 
-createThumbnails(getDataFrom());
+getDataFrom(createThumbnails);
 setUserFormSubmit(() => {
   closeUploadFileForm();
   getSuccess();
